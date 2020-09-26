fb4e7b765332bc13c76896930f839e4b1a1171d5
Aemilus <35335174+Aemilus@users.noreply.github.com>
2020-09-26 22:26:02 +0300

    update

---
+++ Ch.1__A_Tutorial_Introduction/1.9_Character_Arrays/print_longest_input_line.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+#define MAXLINE 1000    /* maximum input line size */
+#define NEWLINE '\n'
+#define NULL    '\0'    /* null character */
+
+int getline(char line[], int maxline);
+void copy(char to[], char from[]);
+
+main()
+{
+    int len;                /* current line length */
+    int max;                /* maximum line length seen so far */
+    char line[MAXLINE];     /* current input line */
+    char longest[MAXLINE];  /* longest line saved here */
+
+    max = 0;
+    while ((len = getline(line, MAXLINE)) > 0)
+        if (len > max) {
+            copy(longest, line);
+            max = len;
+        }
+
+    if (max > 0) {
+        printf("Longest Line:\n");
+        printf("%s", longest);
+    }
+}
+
+/* getline: read a line into s and return it's length */
+int getline(char s[], int lim)
+{
+    char c;
+    int i;
+    
+    for (i=0; i<lim-1 && (c=getchar())!=EOF && c!=NEWLINE; ++i)
+        s[i] = c;
+    if (c == NEWLINE) {
+        s[i] = c;
+        ++i;
+    }
+
+    s[i] = NULL;
+
+    return i;
+}
+
+/* copy: copy 'from' into 'to'; assume 'to' is big enough */
+void copy(char to[], char from[])
+{
+    int i;
+    char c;
+
+    for (i=0; (to[i] = from[i]) != NULL; ++i);
+}
